f41210182d3bf4cc496341ef59aa913da3c90ad7
officialsanju2004 <godsanju21@gmail.com>
2026-01-28 22:19:32 +0530

    Fix environment variable names for email auth

--- server/DataFolder/UserEnquiryFolder.js
+++ server/DataFolder/UserEnquiryFolder.js
@@ -10,8 +10,8 @@ let enquiryInsert = async (req, res) => {
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user:process.env.Email_USER,
+        pass:process.env.Email_pass,
       },
     });
 
